1aa41d204b6812d42baae37133e17300217de761
Satvik <satvik.shrivastava.exe@gmail.com>
2024-10-07 12:45:21 +0530

    Done

--- program1.py
+++ program1.py
@@ -1,6 +1,24 @@
 class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
+        if not grid:
+            return 0
+        
+        def dfs(grid, i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
+                return
+            grid[i][j] = 'W'  
+            dfs(grid, i + 1, j)  
+            dfs(grid, i - 1, j)  
+            dfs(grid, i, j + 1)  
+            dfs(grid, i, j - 1)  
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 'L':
+                    dfs(grid, i, j)
+                    count += 1
+        
+        return count
                     
-        return 0
--- program2.py
+++ program2.py
@@ -1,5 +1,28 @@
 def decode_message( s: str, p: str) -> bool:
 
-# write your code here
-  
-        return False
+        if is_match(s, p):
+                return True
+        else:
+                return False
+
+
+def is_match(message, pattern):
+    m = len(message)
+    p = len(pattern)
+    dp = [[False] * (p + 1) for _ in range(m + 1)]
+    
+    dp[0][0] = True  
+    
+    
+    for j in range(1, p + 1):
+        if pattern[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+    
+    for i in range(1, m + 1):
+        for j in range(1, p + 1):
+            if pattern[j - 1] == '*':
+                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+            elif pattern[j - 1] == '?' or pattern[j - 1] == message[i - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+    
+    return dp[m][p]
